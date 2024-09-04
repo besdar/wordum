9362fb53651fd5e6e02cf7aeddd455971e6365e4
besdar <43372966+besdar@users.noreply.github.com>
2024-09-04 23:01:03 +0400

    version 0.1.4
    - fix export function for android v13 and upper

--- src/shared/lib/permissions.ts
+++ src/shared/lib/permissions.ts
@@ -1,6 +1,10 @@
-import {Permission, PermissionsAndroid} from 'react-native';
+import {Permission, PermissionsAndroid, Platform} from 'react-native';
 
 export const askForPermission = async (permission: Permission) => {
+  if (Number(Platform.Version) >= 33 && permission === PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE) {
+    return true;
+  }
+  
   const isPermissionGranted = await PermissionsAndroid.check(permission);
   if (isPermissionGranted) {
     return;
